9994cdd7d3e9e3bf94892abd8d976696a36a6d2c
miwateranishi <mteranishi@osisoft.com>
2016-12-28 10:24:21 +0900

    Japan Past Weather (cloud cov and wind dir)
    downloads files that contain past data for cloud coverage and wind speed from worldweatheronline.com

---
+++ Japan Weather/WeatherPastDataJapan.py
@@ -0,0 +1,42 @@
+import requests
+import datetime
+CityList=["Tokyo","Yokohama","Kawasaki,Kanagawa","Osaka-Shi","Kyoto","Kobe","Sapporo","Sendai-Shi","Fukuoka-Shi","Nagoya-Shi", "Hiroshima-Shi","Kanazawa-Shi","Kochi-Shi"]
+#Tokyo: Tokyo, Yokohama, Kawasaki
+#Kansai: Osaka-Shi, Kyoto, Kobe
+#Hokkaido: Sapporo
+#Tohoku: Sendai-Shi
+#Kyushu: Fukuoka-Shi
+#Chubu: Nagoya
+#Chugoku: Hiroshima-Shi
+#Hokuriku: Kanazawa-Shi
+#Shikoku: Kochi-Shi
+
+Today=datetime.date.today()
+OneDay=datetime.timedelta(days=1)
+for City in CityList:
+    for x in range(1,10):
+        URL = "http://api.worldweatheronline.com/free/v2/past-weather.ashx?q="+City+",Japan&format=xml&date="+str(Today-x*OneDay)+"&enddate="+str(Today-x*OneDay)+"&tp=1&key=623972a11cd4e1c5e66be37a2d229"
+        print(URL)
+        R= requests.get(URL)
+        if (City=="Kawasaki,Kanagawa"):
+            City="Kawasaki"
+        if (City=="Osaka-Shi"):
+            City="Osaka"
+        if (City=="Sendai-Shi"):
+            City="Sendai"
+        if (City=="Fukuoka-Shi"):
+            City="Fukuoka"
+        if (City=="Hiroshima-Shi"):
+            City="Hiroshima"
+        if (City=="Kanazawa-Shi"):
+            City="Kanazawa"
+        if (City=="Kochi-Shi"):
+            City="Kochi"
+        if (City=="Nagoya-Shi"):
+            City="Nagoya"
+        Q=open("C:\\users\\mteranishi\\Documents\\Japan Weather\\Past Weather Data\\" + City + " " + str(Today-x*OneDay)  + " Past.txt", "w+")
+        Q.write(R.text)
+        Q.close()
+
+
+   
